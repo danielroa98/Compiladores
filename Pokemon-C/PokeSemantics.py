c2499751a01c7d0af6dac70396904633aaecaa46
Daniel <daniel.roa98@gmail.com>
2021-11-18 19:55:41 -0600

    Uploaded latest updates

--- Pokemon-C/PokeSemantics.py
+++ Pokemon-C/PokeSemantics.py
@@ -37,6 +37,12 @@ varCompFlag2 = False
 vars_to_compare = []
 logcial_operator = ''
 
+var1_type = ''
+var1_value = ''
+comparacion = ''
+var2_type = ''
+var2_value = ''
+
 
 def init(token):
     programInit(token)
@@ -82,12 +88,11 @@ def programInit(token):
             return
             # print("Changing the start flag:", start_flag)
     elif start_flag == True:
+        #Estamos haciendo un 
         variables(token)
-        if token.type == 'IF' or token.type == 'WHILE' or if_flag == True or while_loop_flag == True:
-            checkLoop(token)
-            checkConditional(token)
-        # if 'FINISH' in token:
-        elif token.type == 'FINISH':
+        # print(token)
+        checkLoop(token)
+        if token.type == 'FINISH':
             programEnd(token)
     else:
         printErr(token, 1)
@@ -189,7 +194,8 @@ def assign_variable(token):
                 state += 1
         else:
             if token.type != ';':
-                print("Error in line", token.lineno, ": Syntax error")
+                print("Error in line", token.lineno,
+                      ": Syntax error, assign variable")
                 sys.exit(2)
 
     # Ya tenemos la operacion, ahora el valor
@@ -246,6 +252,8 @@ def modify_existing_variable(token):
     global symbol_table
     global current_operator
 
+    #DANIIIIIprint('\nInside the MEV function\n')
+
     # Confirmar asignacion
     if state == 0:
         # Validacion
@@ -300,8 +308,8 @@ def modify_existing_variable(token):
                 current_operator = token.type
                 state += 1
         else:
-            if token.type != ';':
-                print("Error in line", token.lineno, ": Syntax error")
+            if token.type != ';' or if_flag != False:
+                print("Error in line", token.lineno, ": Syntax error, MEV")
                 sys.exit(2)
 
     # Ya tenemos la operacion, ahora el valor
@@ -380,7 +388,8 @@ def variables(token):
             # First we check if the variable already exists
             if checkIfVariableIsDefined(token):
                 # Variable exists! yay
-                modify_existing_varaible_flag = True
+                # DANIIIIprint('\nDani Rojas\n')
+                modify_existing_varaible_flag = Tru`e
                 # Guardar mi variable en mis flags
                 type_flag = symbol_table[token.value]['type']
                 current_variable_ID = token.value
@@ -398,80 +407,144 @@ def checkLoop(token):
     global rightparenthesis
     global logcial_operator
 
-    if token.type == 'IF' or if_flag == True:
-        if_flag = True
-
-        if token.type == '(' or leftparenthesis == True:
-            leftparenthesis = True
-
-            if conditional_op_flag == False:
-                checkConditional()
-            # '(' Check open parenthesis
-                # If true, next we check for a condition
-                # If false we expected a (
-
-                print('This is an if')
-
-    elif token.type == 'WHILE' or while_loop_flag == True:
-        while_loop_flag = True
-        print('This is a while')
+    if if_flag == True or while_loop_flag == True:
+        checkConditional(token)
+    else:
+        if token.type == 'IF':
+            if_flag = True
+            print(if_flag, 'current value')
+        elif token.type == 'WHILE':
+            while_loop_flag = True
 
 
 def checkConditional(token):
-    global varComparator1
-    global varCompFlag1
-    global varComparator2
-    global varCompFlag2
-    global vars_to_compare
-    #if token.type == 'BOOL_TYPE':
+    global var1_type
+    global var1_value
+    global comparacion
+    global var2_type
+    global var2_value
+    global state
+    # if token.type == 'BOOL_TYPE':
     #    conditional_op_flag = True
-        #Regresar
-    #Encontrar todas las variables y operador
-    #Recibir primera variable
-    if token.type=='INT_TYPE':
-        if varCompFlag1 == False:
-            varCompFlag1 = True
-            varComparator1 = 'INT'
-            vars_to_compare.append(token.value)
-        elif varComparator2==False:
-            if conditional_flag==False:
-                
-            varCompFlag2 = True
-            varComparator2 = 'INT'
-            vars_to_compare.append(token.value)
-            #Call the operation
-            logicalOperations(vars_to_compare, operator)
+    # Regresar
+    # Encontrar todas las variables y operador
+    # Recibir primera variable
+
+    # 0. Esperamos un parentesis izquierdo
+    if state == 0:
+        if token.type == '(':
+            state += 1
         else:
-            print("Error, two values already used in the comparison")
-        varComparator1=token.value
+            print("ERROR: Esperaba un (")
 
-    
-    
+    # 1. Espero una variable
+    elif state == 1:
+        # A ver, que variable / valor estamos esperando ?
+        if token.type == 'INT_TYPE' or token.type == 'FLOAT_TYPE':
+            var1_type = token.type
+            var1_value = token.value
+            state += 1
+        elif token.type == 'BOOL_TYPE':
+            var1_type = token.type
+            var1_value = token.value
+            state += 1
+        elif token.type == 'ID':
+            # 1. Checar si ese ID ya existe en mi tabla de simbolos
+            if token.value in symbol_table.keys():
+                print("ID exists! : ", symbol_table[token.value])
+                var1_type = token.type
+                var1_value = token.value
+                state += 1
+            else:
+                print("Error in line", token.lineno, ":  Variable ",
+                      token.value, ' is not defined.')
+                sys.exit(2)
+        else:
+            print("ERROR: Esperaba un valor / variable primer lugar")
+            sys.exit(2)
+
+    # 2. Espero una operacion logica
+    elif state == 2:
+        if token.type == 'EQ' or token.type == 'LE' or token.type == 'GE' or token.type == 'GT' or token.type == 'LT' or token.type == 'NE':
+            comparacion = token.type
+            state += 1
+        elif token.type == ')':
+            state = 5
+        else:
+            print("Error in line", token.lineno, ": Syntax error", token.value)
+            sys.exit(2)
+
+    # 3. Espero mi segunda variable
+    elif state == 3:
+     # A ver, que variable / valor estamos esperando ?
+        if token.type == 'INT_TYPE' or token.type == 'FLOAT_TYPE':
+            if checkValue(token.value, var1_type):
+                var2_type = token.type
+                var2_value = token.value
+                state += 1
+            else:
+                print("ERROR: No son del mismo tipo")
+                sys.exit(2)
+        elif token.type == 'BOOL_TYPE':
+            if checkValue(token.value, var1_type):
+                var2_type = token.type
+                var2_value = token.value
+                state += 1
+            else:
+                print("ERROR: No son del mismo tipo")
+                sys.exit(2)
+        elif token.type == 'ID':
+            # 1. Checar si ese ID ya existe en mi tabla de simbolos
+            if token.value in symbol_table.keys():
+                print("ID exists! : ", symbol_table[token.value])
+                if checkValue(token.value, var1_type):
+                    var2_type = token.type
+                    var2_value = token.value
+                    state += 1
+                else:
+                    print("ERROR: No son del mismo tipo, pelas")
+            else:
+                print("Error in line", token.lineno, ":  Variable ",
+                      token.value, ' is not defined.')
+                sys.exit(2)
+        else:
+            print("ERROR: Esperaba un valor / variable segundo lugar")
+            sys.exit(2)
+
+    # Ahora espero cerrar la comparacion
+    elif state == 4:
+        if token.type == ')':
+            state += 1
+        else:
+            print("ERROR: Esperaba un )")
+            sys.exit(2)
+
+    # Hacer la comparacion, ver si es valida, que te regresa y guardarla en diccionario (poner si vas a empezar if o while)
+    # Resetear todas las banderas declaradas
+    # {'IF': var1 {TYPE: 'INT_TYPE', 'value': 100}, 'var2': {'type': 'INT_TYPE', 'value': 110}, OPERATION}
+    elif state == 5:
+        if logicalOperations(var1_value, var2_value, comparacion):
+            print('Si cumple')
+        else:
+            print('Chole con tus quejas')
 
 
-def logicalOperations(variableList, operator):
+def logicalOperations(var1, var2, operator):
     """ 
     Additional information:
 
-    EQ = r'=='
+    EQ -> r'=='
 
-    LE = r'<='
+    LE -> r'<='
 
-    LT = r'<'
+    LT -> r'<'
 
-    GE = r'>='
+    GE -> r'>='
 
-    GT = r'>'
+    GT -> r'>'
 
-    NE = r'!='
+    NE -> r'!='
     """
-    global vars_to_compare
-
-    var1 = variableList[0]
-    var2 = variableList[1]
-    
-    #Clear variable list after declaration
-    vars_to_compare.clear()
 
     if operator.type == 'EQ':
         if var1 == var2:
@@ -522,3 +595,43 @@ def programEnd(token):
     else:
         printErr(token, 4)
     # return token
+
+""" 
+    TODO:
+        * Arreglar operaciones matematicas (pikachu = x 10);
+        * Diferenciar entre variables
+        * Pura declaracion de arreglos (se pueden manejar como las listas de Python)
+            * Si se hace eso, puede afectar la tabla de simbolos 
+ """
+
+
+""" 
+Token(type='START', value='battle_start', lineno=1, index=0)
+Token(type=';', value=';', lineno=1, index=12)
+pass
+Token(type='INT_TYPE', value='pikachu', lineno=2, index=14)
+Token(type='ID', value='x', lineno=2, index=22)
+Token(type='ASSIGN', value='=', lineno=2, index=24)
+Token(type='INTEGER', value=0, lineno=2, index=26)
+Token(type=';', value=';', lineno=2, index=27)
+Finished! x INT_TYPE 0
+{'x': {'type': 'INT_TYPE', 'value': 0}}
+pass
+Token(type='INT_TYPE', value='pikachu', lineno=3, index=29)
+Token(type='ID', value='end', lineno=3, index=37)
+Token(type='ASSIGN', value='=', lineno=3, index=41)
+Token(type='INTEGER', value=10, lineno=3, index=43)
+Token(type=';', value=';', lineno=3, index=45)
+Finished! end INT_TYPE 10
+{'x': {'type': 'INT_TYPE', 'value': 0}, 'end': {'type': 'INT_TYPE', 'value': 10}}
+pass
+Token(type='IF', value='if_i_choose_you', lineno=4, index=47)
+pass
+Token(type='(', value='(', lineno=4, index=63)
+pass
+Token(type='ID', value='x', lineno=4, index=64)
+Token(type='LE', value='<=', lineno=4, index=66)
+Syntax error mod 0
+
+{'IF': {'var1': VALUE, 'var2': VALUE, 'type': 'TIPO_OPERACION'}}
+ """
